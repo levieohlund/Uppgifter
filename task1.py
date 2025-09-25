c6f8aceb16b942b9281064f02021cf5fc4e1afba
levieohlund-lang <levie.ohlund@gmail.com>
2025-09-25 17:12:55 +0200

    Uppdaterade task1.py

---
+++ task1.py
@@ -0,0 +1,13 @@
+#lista med favorit filmer 
+
+movies_list = ['Scarface', 'Rambo', 'Titanic']
+# Lägg till en film med append()
+movies_list.append('Seven')
+
+print(movies_list)
+for movie in movies_list:
+    print(movie)
+print("Första filmen:", movies_list[0])
+print("Sista filmen:", movies_list[-1])
+
+movies_list.append ("scarface", "Seven")
